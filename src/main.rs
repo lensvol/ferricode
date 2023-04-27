5aecefb97158ad8dd7b71d0a0332acbf2b605bde
Kirill Borisov <lensvol@gmail.com>
2023-04-27 11:20:22 +0200

    Implement support for 'infinite' memory.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 use std::fmt::{Display, Formatter};
+use std::ops::Range;
 
 #[derive(Debug)]
 enum OpCode {
@@ -114,8 +115,10 @@ impl Display for OpCode {
     }
 }
 
+type ComputerMemory = HashMap<usize, i32>;
+
 struct Computer {
-    memory: Vec<i32>,
+    memory: ComputerMemory,
     instruction_pointer: usize,
     relative_base: usize,
     input: VecDeque<i32>,
@@ -123,7 +126,12 @@ struct Computer {
 }
 
 impl Computer {
-    fn new(memory: Vec<i32>, input: Vec<i32>) -> Computer {
+    fn new(initial_memory: Vec<i32>, input: Vec<i32>) -> Computer {
+        let mut memory = HashMap::new();
+        for (i, value) in initial_memory.into_iter().enumerate() {
+            memory.insert(i, value);
+        }
+
         Computer {
             memory,
             instruction_pointer: 0,
@@ -133,14 +141,23 @@ impl Computer {
         }
     }
 
+    fn read_memory(&self, addr: usize) -> i32 {
+        *self.memory.get(&addr).unwrap_or(&0)
+    }
+
+    fn write_memory(&mut self, addr: usize, value: i32) {
+        self.memory.insert(addr, value);
+    }
+
     fn read_addr(&mut self, mode: &ParameterMode) -> usize {
+        let value = self.read_memory(self.instruction_pointer);
+        self.instruction_pointer += 1;
+
         let addr = match mode {
             ParameterMode::Position | ParameterMode::Immediate => {
-                let value = self.memory[self.instruction_pointer];
-                self.instruction_pointer += 1;
                 value
             },
-            ParameterMode::Relative => (self.instruction_pointer + self.relative_base) as i32
+            ParameterMode::Relative => self.relative_base as i32 + value
         };
 
         if addr < 0 {
@@ -153,19 +170,19 @@ impl Computer {
     fn read_value(&mut self, mode: &ParameterMode) -> i32 {
         return match mode {
             ParameterMode::Immediate => {
-                let value = self.memory[self.instruction_pointer];
+                let value = self.read_memory(self.instruction_pointer);
                 self.instruction_pointer += 1;
                 value
             }
             _ => {
                 let addr = self.read_addr(mode);
-                self.memory[addr]
+                self.read_memory(addr)
             }
         }
     }
 
     fn step_forward(&mut self) -> bool {
-        let instruction = Instruction::try_from(self.memory[self.instruction_pointer])
+        let instruction = Instruction::try_from(self.read_memory(self.instruction_pointer))
             .expect("Failed to decode instruction");
 
         print!("[IP: {} RB: {}] ", self.instruction_pointer, self.relative_base);
@@ -183,7 +200,7 @@ impl Computer {
                 let addr = self.read_addr(&instruction.parameter_modes[2]);
 
                 println!("ADD {} {} {}", arg1, arg2, addr);
-                self.memory[addr] = arg1 + arg2;
+                self.write_memory(addr,  arg1 + arg2);
             }
             OpCode::Mul => {
                 let arg1 = self.read_value(&instruction.parameter_modes[0]);
@@ -191,14 +208,14 @@ impl Computer {
                 let addr = self.read_addr(&instruction.parameter_modes[2]);
 
                 println!("ADD {} {} {}", arg1, arg2, addr);
-                self.memory[addr] = arg1 * arg2;
+                self.write_memory(addr,  arg1 * arg2);
             }
             OpCode::Input => {
                 let addr = self.read_addr(&instruction.parameter_modes[0]);
                 let input = self.input.pop_front().expect("Input exhausted!");
 
                 println!("IN {} {}", input, addr);
-                self.memory[addr] = input;
+                self.write_memory(addr, input);
             }
             OpCode::Output => {
                 let value = self.read_value(&instruction.parameter_modes[0]);
@@ -230,7 +247,7 @@ impl Computer {
                 let addr = self.read_addr(&instruction.parameter_modes[2]);
 
                 println!("SLT {} {} {}", arg1, arg2, addr);
-                self.memory[addr] = if arg1 < arg2 { 1 } else { 0 };
+                self.write_memory(addr, if arg1 < arg2 { 1 } else { 0 });
             }
             OpCode::StoreIfEquals => {
                 let arg1 = self.read_value(&instruction.parameter_modes[0]);
@@ -238,7 +255,7 @@ impl Computer {
                 let addr = self.read_addr(&instruction.parameter_modes[2]);
 
                 println!("SEQ {} {} {}", arg1, arg2, addr);
-                self.memory[addr] = if arg1 == arg2 { 1 } else { 0 };
+                self.write_memory(addr, if arg1 == arg2 { 1 } else { 0 });
             }
             OpCode::IncrementRelativeBase => {
                 let offset = self.read_value(&instruction.parameter_modes[0]);
@@ -277,8 +294,26 @@ fn main() {
     println!("Output: {:?}", computer.output);
 }
 
+trait RangeAddressable {
+    fn read_range(&self, range: Range<usize>) -> Vec<i32>;
+    fn write_range(&mut self, range: Range<usize>, data: Vec<i32>);
+}
+
+impl RangeAddressable for ComputerMemory {
+    fn read_range(&self, range: Range<usize>) -> Vec<i32> {
+        range
+            .into_iter()
+            .map(|addr| *self.get(&addr).unwrap_or(&0))
+            .collect()
+    }
+
+    fn write_range(&mut self, _range: Range<usize>, _data: Vec<i32>) {
+        todo!()
+    }
+}
+
 mod tests {
-    use crate::Computer;
+    use crate::{Computer, RangeAddressable};
 
     fn run_program(program: Vec<i32>, input: Vec<i32>) -> Computer {
         let mut computer = super::Computer::new(program, input);
@@ -289,16 +324,16 @@ mod tests {
     #[test]
     fn test_day_2_simple_programs() {
         let computer = run_program(vec![1, 0, 0, 0, 99], vec![]);
-        assert_eq!(computer.memory, vec![2, 0, 0, 0, 99]);
+        assert_eq!(computer.memory.read_range(0..5), vec![2, 0, 0, 0, 99]);
 
         let computer = run_program(vec![2, 3, 0, 3, 99], vec![]);
-        assert_eq!(computer.memory, vec![2, 3, 0, 6, 99]);
+        assert_eq!(computer.memory.read_range(0..5), vec![2, 3, 0, 6, 99]);
 
         let computer = run_program(vec![2, 4, 4, 5, 99, 0], vec![]);
-        assert_eq!(computer.memory, vec![2, 4, 4, 5, 99, 9801]);
+        assert_eq!(computer.memory.read_range(0..6), vec![2, 4, 4, 5, 99, 9801]);
 
         let computer = run_program(vec![1, 1, 1, 4, 99, 5, 6, 0, 99], vec![]);
-        assert_eq!(computer.memory, vec![30, 1, 1, 4, 2, 5, 6, 0, 99]);
+        assert_eq!(computer.memory.read_range(0..9), vec![30, 1, 1, 4, 2, 5, 6, 0, 99]);
     }
 
     #[test]
@@ -307,16 +342,16 @@ mod tests {
         assert_eq!(computer.output, vec![42]);
 
         let computer = run_program(vec![1002, 4, 3, 4, 33], vec![]);
-        assert_eq!(computer.memory, vec![1002, 4, 3, 4, 99]);
+        assert_eq!(computer.memory.read_range(0..5), vec![1002, 4, 3, 4, 99]);
 
         let computer = run_program(vec![1101, 100, -1, 4, 0], vec![]);
-        assert_eq!(computer.memory, vec![1101, 100, -1, 4, 99]);
+        assert_eq!(computer.memory.read_range(0..5), vec![1101, 100, -1, 4, 99]);
     }
 
     #[test]
     fn test_day_9_simple_programs() {
-        let computer = run_program(vec![109, 19, 204, -34, 99], vec![]);
-        assert_eq!(computer.output, vec![19]);
+        let computer = run_program(vec![109, 2000, 204, -34, 99], vec![]);
+        assert_eq!(computer.output, vec![0]);
 
         let computer = run_program(vec![109, 1, 9, 2, 204, -6, 99], vec![]);
         assert_eq!(computer.output, vec![204]);
